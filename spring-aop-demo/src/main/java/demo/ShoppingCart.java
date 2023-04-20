880afcd114b696b20dc2a6da13016dd176a31e3f
ayush27prasad <ayushprasadcool@gmail.com>
2023-04-20 22:15:08 +0530

    Initial Commit

---
+++ spring-aop-demo/src/main/java/demo/ShoppingCart.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+
+
+    public void checkout(){
+        System.out.println("Checkout method from Shopping Cart is called!");
+    }
+
+}
